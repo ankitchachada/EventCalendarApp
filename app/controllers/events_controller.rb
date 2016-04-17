a57b1442b0948cd35efcb3e489387a127dd7d3ba
ankit <ankit.chachada@gmail.com>
2016-04-16 17:59:33 -0700

    like feature initiated

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -61,6 +61,10 @@ class EventsController < ApplicationController
     end
   end
 
+  def like
+    #code..
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_event
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
-  resources :events
+  resources :events do
+    member do
+      post 'like'
+    end
+  end
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
