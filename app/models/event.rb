5cfde96349475cf0feea50bafe5921a60b0c8207
ankit <ankit.chachada@gmail.com>
2016-05-03 14:19:39 -0700

    custom validation added

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,11 @@
 class Event < ActiveRecord::Base
    belongs_to :user
    has_many :likes
+   validate :date_of_event
+
+   def date_of_event
+   	  if event_date < Time.now.utc
+   	  	errors.add(:event_date, "can't be in the past")
+   	  end
+   end
 end
